073d68f1148971151aec3afa2442e5b641e02c0f
soamtripathy <soumyaranjantripathy44@gmail.com>
2023-05-24 12:05:06 +0530

    updated: textarea and length

--- app.py
+++ app.py
@@ -10,27 +10,32 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/summary',methods=['POST'])
+@app.route('/summary', methods=['POST'])
 def summary_api():
-    url=request.form["url"]
+    url = request.form["url"]
     video_id = url.split('=')[-1]
-    transcript=get_transcript(video_id)
+    transcript = get_transcript(video_id)
     summary = get_summary(transcript)
-    return render_template("index.html",transcript=transcript,summary=summary)
+    length_transcript = len(transcript)
+    summary_length = len(summary)
+    return render_template("index.html", transcript=transcript, summary=summary, length_transcript=length_transcript, summary_length=summary_length)
+
 
 def get_transcript(video_id):
     transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
     transcript = ' '.join([d['text'] for d in transcript_list])
     return transcript
 
+
 def get_summary(transcript):
     summariser = pipeline('summarization')
     summary = ''
     for i in range(0, (len(transcript)//1000)+1):
-        summary_text = summariser(transcript[i*1000:(i+1)*1000])[0]['summary_text']
+        summary_text = summariser(
+            transcript[i*1000:(i+1)*1000])[0]['summary_text']
         summary = summary + summary_text + ' '
     return summary
-    
+
 
 if __name__ == '__main__':
     app.run(port=8000)
